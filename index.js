a77559ff0bc360c206f9b63cb0c8ce1b958ad9e4
olaoluwaigun <igunolly@gmail.com>
2025-03-04 06:04:12 -0500

    Done

---
+++ index.js
@@ -0,0 +1,47 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Please enter a task.");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.style.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+  localStorage.setItem("taskList", listContainer.innerHTML);
+}
+
+function loadData() {
+  listContainer.innerHTML = localStorage.getItem("taskList");
+}
+loadData();
+
+// Execute a function when the user presses a key on the keyboard
+inputBox.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("addBtn").click();
+  }
+});
